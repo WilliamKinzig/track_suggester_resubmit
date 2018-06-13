4d77dfd1c7ed901d47ea8dfcaa9cee170584562b
William Kinzig <wippawill2@gmail.com>
2018-06-12 19:02:18 -0700

    add borders begin to reconfigure logic of the js

---
+++ js/scripts.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+  $("form#questionaire").submit(function(event) {
+    //var age = parseInt($("input#age").val());
+    var option = parseInt($("select#option").val());
+
+    if (option === 1) {
+      var course = "CSS";
+      $("#fill").empty().append(course);
+      $("#recommendation").show();
+    }else if (option === 2) {
+      var course = "c#";
+      //$("#fill").empty().append(course);
+      $("#recommendation").show();
+    }else if (option === 3) {
+      var course = "Java";
+      //$("#fill").empty().append(course);
+      $("#recommendation").show();
+    }else if (option === 4) {
+      var course = "PHP";
+      //$("#fill").empty().append(course);
+      $("#recommendation").show();
+    }else (option === 5) {
+      var course = "RUBY";
+      //$("#fill").empty().append(course);
+      $("#recommendation").show();
+    }
+
+
+    event.preventDefault();
+  });
+});
