67baf5c884eeaef4c503973b76b32563415eca65
William Kinzig <wippawill2@gmail.com>
2018-06-12 20:12:26 -0700

    create js variables to take in value from radio boxes

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,40 @@
 $(document).ready(function() {
   $("form#questionaire").submit(function(event) {
-    //var age = parseInt($("input#age").val());
     var option = parseInt($("select#option").val());
 
-    if (option === 1) {
-      var course = "CSS";
-      $("#fill").empty().append(course);
-      $("#recommendation").show();
-    }else if (option === 2) {
-      var course = "c#";
-      //$("#fill").empty().append(course);
-      $("#recommendation").show();
-    }else if (option === 3) {
-      var course = "Java";
-      //$("#fill").empty().append(course);
-      $("#recommendation").show();
-    }else if (option === 4) {
-      var course = "PHP";
-      //$("#fill").empty().append(course);
-      $("#recommendation").show();
-    }else (option === 5) {
-      var course = "RUBY";
-      //$("#fill").empty().append(course);
-      $("#recommendation").show();
-    }
-    #quote.show();
     event.preventDefault();
+
+    var option1 = $("input:radio[name=quesGroup01]:checked").val();
+    var option2 = $("input:radio[name=quesGroup02]:checked").val();
+    var option3 = $("input:radio[name=quesGroup03]:checked").val();
+
   });
 });
+
+//var age = parseInt($("input#age").val());
+
+// if (option === 1) {
+//   var course = "CSS";
+//   $("#fill").empty().append(course);
+//   $("#recommendation").show();
+// }else if (option === 2) {
+//   var course = "c#";
+//   //$("#fill").empty().append(course);
+//   $("#recommendation").show();
+// }else if (option === 3) {
+//   var course = "Java";
+//   //$("#fill").empty().append(course);
+//   $("#recommendation").show();
+// }else if (option === 4) {
+//   var course = "PHP";
+//   //$("#fill").empty().append(course);
+//   $("#recommendation").show();
+// }else (option === 5) {
+//   var course = "RUBY";
+//   //$("#fill").empty().append(course);
+//   $("#recommendation").show();
+// }
+
+// $("button").click(function() {
+//   $("quote").show();
+// });
