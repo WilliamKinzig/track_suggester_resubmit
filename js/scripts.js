a303d657fb9f240d33144eb832085cd996f72583
William Kinzig <wippawill2@gmail.com>
2018-06-13 21:58:26 -0700

    add button click function

--- js/scripts.js
+++ js/scripts.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
       var option2 = parseInt($("input:radio[name=quesGroup02]:checked").val());
       var option3 = parseInt($("input:radio[name=quesGroup03]:checked").val());
 
-      var total = option1 + option2 + option3;
+      //var total = option1 + option2 + option3;
 
       // var bob = 0;
       // if(total === 7){
@@ -14,11 +14,24 @@ $(document).ready(function() {
       //   bob = 1;
       // }else if(total === 6){
       //   bob = 2;
-      // }else if(totol === 5){
+      // }else if(total === 5){
       //   bob = 3;
       // }else {
       //   bob = 4;
       // }
+      //
+      // if (bob === 1){
+      //   //$(".cssReact").show();
+      // }else if (bob === 2){
+      //   //$(".rubyRails").show();
+      // }else(
+      //   //$(".cNet").show;
+      // )
+
+
+      // Ruby/Rails
+      // CSS/React
+      // C#/.NET
 
      $(".summary").show();
   });
