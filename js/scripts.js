36360fa5b6eb1c34ca373e703eec47bc8a5a871c
William Kinzig <wippawill2@gmail.com>
2018-06-26 22:18:02 -0700

    push to the point where you need help

--- js/scripts.js
+++ js/scripts.js
@@ -6,27 +6,27 @@ $(document).ready(function() {
       var option2 = parseInt($("input:radio[name=quesGroup02]:checked").val());
       var option3 = parseInt($("input:radio[name=quesGroup03]:checked").val());
 
-      //var total = option1 + option2 + option3;
+      var total = option1 + option2 + option3;
 
-      // var bob = 0;
-      // if(total === 7){
-      // $("#summary").show();
-      //   bob = 1;
-      // }else if(total === 6){
-      //   bob = 2;
-      // }else if(total === 5){
-      //   bob = 3;
-      // }else {
-      //   bob = 4;
-      // }
-      //
-      // if (bob === 1){
-      //   //$(".cssReact").show();
-      // }else if (bob === 2){
-      //   //$(".rubyRails").show();
-      // }else(
-      //   //$(".cNet").show;
-      // )
+       var total = 0;
+       if(total === 0){
+       $("#summary").show()};
+//         bob = 1;
+//       }else if(total === 6){
+//         bob = 2;
+//       }else if(total === 5){
+//         bob = 3;
+//       }else {
+//         bob = 4;
+//       }
+//      
+//       if (bob === 1){
+//         $(".cssReact").show();
+//       }else if (bob === 2){
+//         $(".rubyRails").show();
+//       }else(
+//         $(".cNet").show;
+//       )
 
 
       // Ruby/Rails
