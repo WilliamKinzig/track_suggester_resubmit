637da9ddf1bab35e60a2909e39b2fc0f5e355881
William Kinzig <wippawill2@gmail.com>
2018-06-27 21:44:07 -0700

    make button function properly

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,45 @@
-$(document).ready(function() {
-  $("#button").click(function(){
+$(document).ready(function () {
+  $("#button").click(function () {
     event.preventDefault();
 
-      var option1 = parseInt($("input:radio[name=quesGroup01]:checked").val());
-      var option2 = parseInt($("input:radio[name=quesGroup02]:checked").val());
-      var option3 = parseInt($("input:radio[name=quesGroup03]:checked").val());
+    var option1 = parseInt($("input:radio[name=quesGroup01]:checked").val());
+    var option2 = parseInt($("input:radio[name=quesGroup02]:checked").val());
+    var option3 = parseInt($("input:radio[name=quesGroup03]:checked").val());
 
-      var total = option1 + option2 + option3;
+    var total = option1 + option2 + option3;
+    var finalScore = 0;
 
-       var total = 0;
-       if(total === 0){
-       $("#summary").show()};
-//         bob = 1;
-//       }else if(total === 6){
-//         bob = 2;
-//       }else if(total === 5){
-//         bob = 3;
-//       }else {
-//         bob = 4;
-//       }
-//      
-//       if (bob === 1){
-//         $(".cssReact").show();
-//       }else if (bob === 2){
-//         $(".rubyRails").show();
-//       }else(
-//         $(".cNet").show;
-//       )
+    if (total === 0) {
+      finalScore = 1;
+    } else if (total === 6) {
+      finalScore = 2;
+    } else if (total === 5) {
+      finalScore = 3;
+    } else {
+      finalScore = 4;
+    }
+
+    if (finalScore === 1) {
+      $(".cssReact").show();
+      showSummary();
+    } else if (finalScore === 2) {
+      $(".rubyRails").show();
+      showSummary();
+    } else {
+      $(".cNet").show;
+      showSummary();
+    }
 
 
-      // Ruby/Rails
-      // CSS/React
-      // C#/.NET
 
-     $(".summary").show();
+    function showSummary(){
+      $("#summary").show();
+    }
+
+    //showSummary();
+    // Ruby/Rails
+    // CSS/React
+    // C#/.NET
+
   });
 });
