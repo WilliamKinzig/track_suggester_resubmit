14cc25e229424fb0bd5b27a54bbbcb84143e7094
WilliamKinzig <wippawill2@gmail.com>
2018-07-01 13:33:06 -0700

    finish resubmit

--- js/scripts.js
+++ js/scripts.js
@@ -6,39 +6,30 @@ $(document).ready(function () {
     var option2 = parseInt($("input:radio[name=quesGroup02]:checked").val());
     var option3 = parseInt($("input:radio[name=quesGroup03]:checked").val());
 
+    var total = 0;
+
     var total = option1 + option2 + option3;
     var finalScore = 0;
 
-    showSummaryRuby();
-
-    if (total === 1) {
+    if (total === 1 || total === 4) {
       finalScore = 1;
-      showSummaryRuby();
-      showSummary();
-    } else if (total === 2) {
+    } else if (total === 2 || total === 5) {
       finalScore = 2;
-      showSummaryReact();
-      showSummary();
-    } else if (total === 3) {
+    } else if (total === 3 || total === 6) {
       finalScore = 3;
-      showSummarySharp();
-      showSummary();
     } else {
       finalScore = 0;
     }
 
-    // if (finalScore === 1) {
-    //   showSummaryRuby();
-    //   showSummary();
-    // } else if (finalScore === 2) {
-    //   showSummaryReact();
-    //   showSummary();
-    // } else if (finalScore ===3){
-    //   showSummarySharp();
-    //   showSummary();
-    // } else {
-    //   finalScore == 0;
-    // }
+    if (finalScore === 1) {
+      showSummaryRuby();
+    } else if (finalScore === 2) {
+      showSummaryReact();
+    } else if (finalScore ===3){
+      showSummarySharp();
+    } else {
+      alert("You must check one box for each section. Refresh the page and try again");
+    }
 
     function showSummaryRuby(){
       $("#summary").show();
@@ -52,11 +43,5 @@ $(document).ready(function () {
       $("#summary").show();
       $("#sharp").show();
     }
-
-    //showSummary();
-    // Ruby/Rails
-    // CSS/React
-    // C#/.NET
-
   });
 });
